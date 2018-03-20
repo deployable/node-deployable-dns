eafa8a71d50ab65595ac02114711371804f3c8f4
Matt Hoyle <code@deployable.co>
2018-03-20 00:06:40 +0000

    up to date, start adding redirect

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-from mhart/alpine-node:6.9.4
+from mhart/alpine-node:6.11.0
+
+arg key
 
 add package.json /
 run npm install -d --production
 add . /
 
 env DEBUG='dply:dnsd:*'
-cmd ["node","lib/dnsd.js"]
+#cmd ["node","lib/dnsd.js"]
+cmd npm start
 
--- test/fixture/mocha-setup.js
+++ test/fixture/mocha-setup.js
@@ -1,11 +1,11 @@
 global.chai = require('chai')
-global.sinon = require('sinon')
+//global.sinon = require('sinon')
 global.expect = chai.expect
 
-require('bluebird').config({
-  longStackTraces: true,
-  warnings: true
-})
+//require('bluebird').config({
+//  longStackTraces: true,
+//  warnings: true
+//})
 
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'test'
 
